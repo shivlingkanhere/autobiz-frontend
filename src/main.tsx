d64b9c9d6f6a6791c68807215ee3010e7d106d1d
Shubham Kanhere <kanhereshubham0@gmail.com>
2026-06-29 01:21:28 +0530

    fix: clean main.tsx and set base url properly

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "./index.css"; // ðŸ‘ˆ THIS WAS MISSING
+import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { setBaseUrl } from "@workspace/api-client-react";
 
 const queryClient = new QueryClient();
+
+// IMPORTANT: API BASE URL SET
+setBaseUrl(import.meta.env.VITE_API_URL);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
